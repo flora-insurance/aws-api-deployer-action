4f0cfce7a5732b9a51129ec501519b18307e5cd6
michael-stappers <58463286+michael-stappers@users.noreply.github.com>
2021-05-19 17:39:51 +0200

    wip : adaptation to swagger 2.0

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const AWS = require('aws-sdk');
 const ApiGtw = require('./lib/api-gtw');
 const ApiMerger = require('./lib/api-merger');
 const deploy = require('./lib/deploy');
-const deployDev = require('./lib/deploy-dev');
 
 async function run() {
   try { 
@@ -14,7 +13,7 @@ async function run() {
     const apiName = core.getInput('api-name') || 'Test githubaction API v1';
     const region = core.getInput('aws-region') || 'eu-west-1';
     const basePath = core.getInput('api-base-path') || 'test1-exp-v1';
-    const domainName = core.getInput('api-domain-name') || 'dev.openapi.ethias.be';
+    const domainName = core.getInput('api-domain-name') || 'internal.api.dev.flora.insure';
     const mediaTypes = core.getInput('api-media-types').split("\n") || [];
     const additionalHeaders = core.getInput('api-additional-headers') || '';
     
@@ -25,33 +24,29 @@ async function run() {
 
     const localSwagger = JSON.parse(fs.readFileSync(swaggerPath));
     
-    if (targetEnv === 'dev') {
-      importedApi = await deployDev({ localSwagger, apiName, basePath, mediaTypes, additionalHeaders });
-    } else {
-      importedApi = await deploy({ localSwagger, apiName });
-    }
+    importedApi = await deploy({ localSwagger, apiName, basePath, mediaTypes, additionalHeaders });
 
     console.log("================== Imported API", JSON.stringify(importedApi, null, 2));
     
-    // Deploy the API on default stage
-    const deployedApi = await apiGtw.createDeployment(importedApi.id, "Test githubaction", "default", "Default");
-    console.log("================== Deployed API", JSON.stringify(deployedApi, null, 2));
+    // // Deploy the API on default stage
+    // const deployedApi = await apiGtw.createDeployment(importedApi.id, "Test githubaction", "default", "Default");
+    // console.log("================== Deployed API", JSON.stringify(deployedApi, null, 2));
 
-    // Associate a web ACL to the stage
-    let webAcl = await apiGtw.updateWebAcl(importedApi.id, domainName);
-    console.log("================== Web ACL", JSON.stringify(webAcl, null, 2));
+    // // Associate a web ACL to the stage
+    // let webAcl = await apiGtw.updateWebAcl(importedApi.id, domainName);
+    // console.log("================== Web ACL", JSON.stringify(webAcl, null, 2));
 
-    // Add stage variable and enable logs
-    let stage = await apiGtw.updateStage(importedApi.id, domainName);
-    console.log("================== Stage", JSON.stringify(stage, null, 2));
+    // // Add stage variable and enable logs
+    // let stage = await apiGtw.updateStage(importedApi.id, domainName);
+    // console.log("================== Stage", JSON.stringify(stage, null, 2));
 
-    // Associate API to custom domain name + base path
-    let basePathMapping = await apiGtw.getBasePathMapping(basePath, domainName);
-    if (!basePathMapping) {
-      basePathMapping = await apiGtw.createBasePathMapping(importedApi.id, basePath, domainName);
-    }
+    // // Associate API to custom domain name + base path
+    // let basePathMapping = await apiGtw.getBasePathMapping(basePath, domainName);
+    // if (!basePathMapping) {
+    //   basePathMapping = await apiGtw.createBasePathMapping(importedApi.id, basePath, domainName);
+    // }
 
-    console.log("================== basePathMapping", JSON.stringify(basePathMapping, null, 2));
+    // console.log("================== basePathMapping", JSON.stringify(basePathMapping, null, 2));
   } 
   catch (error) {
     core.setFailed(error.message);
